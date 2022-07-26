8e4ccdf2aefc4aeee8a1c9bbf773b30235c68d79
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-26 16:52:47 +0200

    Complete Pokemon component

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -4,22 +4,27 @@ import "../components/pokemon.css";
 
 const Pokemon = () => {
     const [pokemon, setPokemon] = useState([]);
-    const getPokeAPI = () => {
-        axios.get("https://pokeapi.co/api/v2/pokemon/133")
+    const [pokemonId, setPokemonId] = useState(1);
+
+    const getPokeAPI = (id) => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then(response => {
-            setPokemon(response.data)
+            setPokemon(response.data);
+            setPokemonId(id);
         })
     }
 
     useEffect(() => {
-        getPokeAPI();
-    }, [])
+        getPokeAPI(pokemonId);
+    }, [pokemonId])
 
   return (
     <div className='pokemon-container'>
         <p>Hello trainer ! You just captured {pokemon.name} !</p>
         <p>The n°{pokemon.id} in the Pokedex. </p>
         <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt="name of the Pokemon"/>
+        <button id="button-previous" type='button' onClick={() => getPokeAPI(pokemonId - 1)}>Previous</button>
+        <button id="button-next" type='button' onClick={() => getPokeAPI(pokemonId + 1)}>Next</button>
     </div>
   )
 }
