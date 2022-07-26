4d1b6345c187cbb40991c013fceec7c8a8c127e8
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-07-26 17:13:49 +0200

    Add Styled components package and stylize Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import "../components/header.css";
+import styled from 'styled-components';
+
 import team1 from "../assets/team1.png";
 import team2 from "../assets/team2.png";
 import team3 from "../assets/team3.png";
 
+const Container = styled.div `
+display: flex;
+    align-items: center;
+    justify-content: space-around;
+    `
+
+const Image = styled.img `
+filter: drop-shadow(0 0 0.75rem rgb(220, 18, 124));
+    width: 15rem;
+    height: 15rem;
+`
+
 const Header = () => {
   return (
-    <div className='header-pokemon'>
-        <img className="header-img" src={team1} alt="team 1"/>
-        <img className="header-img" src={team2} alt="team 2"/>
-        <img className="header-img" src={team3} alt="team 3"/>
-    </div>
+    <Container>
+        <Image src={team1} alt="team 1"/>
+        <Image src={team2} alt="team 2"/>
+        <Image src={team3} alt="team 3"/>
+    </Container>
   )
 }
 
